23e7179e90e1f0f4b1834c864b4a11f78cc1401a
WeronikaR <weronika.redlinska@kainos.com>
2023-11-03 10:52:14 +0100

    feat: application with adding, editing, filtering and listing tasks

---
+++ ToDoApp/Models/ToDoAppContext.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using ToDoApp.Models;
+
+
+namespace ToDoApp.Models{
+    public class ToDoAppContext : DbContext{
+        public ToDoAppContext(DbContextOptions<ToDoAppContext> options) : base(options){ }
+
+        public DbSet<ToDo> ToDos {get; set;} = null!;
+        public DbSet<Category> Categories {get; set;} = null!;
+
+        public DbSet<Status> Statuses {get; set;} = null!;
+
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {  base.OnModelCreating(modelBuilder);
+
+           modelBuilder.Entity<Category>().HasData(
+            new Category {CategoryId = "work", Name="Work"},
+            new Category {CategoryId = "home", Name="Home"},
+            new Category {CategoryId = "exercise", Name="Exercise"}
+            );
+
+            modelBuilder.Entity<Status>().HasData(
+            new Status {StatusId="open", Name="Open"},
+            new Status {StatusId="closed", Name="Completed"}
+            );
+
+        }
+
+    }
+}
